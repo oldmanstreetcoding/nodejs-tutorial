56282b60853aa708f6cc2f58934458b2ded373f4
Street Coding <oldmanstreetcoding@gmail.com>
2021-07-29 19:51:40 +0700

    using npm validator, chalk and nodemon library

---
+++ npm/app.js
@@ -0,0 +1,6 @@
+const validator = require('validator');
+const chalk = require('chalk');
+
+// console.log(validator.isEmail('a@gmail.com'));
+// console.log(validator.isMobilePhone('081247651817', 'id-ID'));
+console.log(chalk.white.bgYellow('Hello world'));
